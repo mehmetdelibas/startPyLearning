a2b64fa0b881e68bdf124760b1dd62ed7b215923
mehmetdelibas <m.metiin7@gmail.com>
2025-06-05 23:58:55 +0300

    learnedBasicStringTerms

--- basicTerms/lessonsTry.py
+++ basicTerms/lessonsTry.py
@@ -41,10 +41,84 @@ import random
 # text="python is weird"
 # print(len(text))
 
-#in ile lsitenin içinde aranan kelımenın varlıgı sorgulanır
-text="The best lanugage in life are free!"
-search="Th"
-if search in text:
-    print("Yes text have")
+# #in ile lsitenin içinde aranan kelımenın varlıgı sorgulanır
+# text="The best lanugage in life are free!"
+# search="Th"
+# if search in text:
+#     print("Yes text have")
+# else:
+#     print("No have")
+
+#modify string
+
+# text = "Python is doing well"
+# print(text.upper())
+# print(text.lower())
+
+# #bas ve sondakı bosluklari kaldırmak için gerkelı olan fonk
+# print(text.strip()) 
+# print(text.replace("P","A"))
+# #ayırmak için
+# print(text.split(" "))
+
+# age=23
+# name = "metin"
+# # text = "Name = {0}, Age = {1}".format(name,age)
+# # print(text)
+# text = f"Name = {name}, Age = {age}"
+# print(text)
+
+# vize = float(input("vize notu giriniz :"))
+# final = float(input("final notu giriniz :"))
+# sonuc = (vize*0.4+final*0.6)
+# if sonuc > 35:
+#     print("geçti")
+# else:
+#     print("kaldi")
+# print(sonuc)
+# upper=> hepsi buyuk lowe => hepsi kucuk captitalize=>ilk harfi büyük
+# casefold=> lowerden daha cok harfı donusturur lowerden daha yavas calısır
+# title=> her kelimenin ilk harfi buyuk olur numarlardan sonra da buyuk başlar
+# swapcase=> buykluklerı degistiri
+# islowe=>isupper=> hepsi buyuk veya kucukse true olur
+# center()=> stringi ortalar 
+# text = "Welcome to my world"
+# resılt = text.center(30,"-")
+# print(resılt)
+
+# count=> arnan şeyin strtinge kaç adet oldugunu gosterıor
+# enswith startswith başlangıcta verdiign degerle baslıyorsa veya bitiyorsa true
+# expandtabs=> \₺ boşluklarını defult8 alır ve () içine rakam yazarak ayarlarsım
+# find=> yazılan harf veya kelımenın indexini dödürür
+# isalnum stringedkiler harf sayi veya harf+sayisya tru doner
+# isalpha=> tum liste harfse tru doner
+# isascii=> tum liste asciyse true doner
+# isdecimal=> isdigit=> sadece rakamsa true doner
+
+# text = "m\t\te\t\ti\tn"
+# result=text.expandtabs(2)
+# print(result)
+# a,b=5,1
+# if a<b:
+#     print("a small b")
+# elif a==b:
+#     print("a equals b")
+# else:
+#     print("a big b")
+# leftshift
+# x=16
+# x <<=2
+# print(x)
+# x=7
+# x |= 3
+# print(x)
+# walrus atama
+# x=7
+# print(x:=19)
+
+psw = 12345
+myPsw = int(input("enter psw :"))
+if psw == myPsw:
+    print("succesful")
 else:
-    print("No have")
+    print("wrong")
