6eab86a548c6a4287178e8f63b5bd9ac037cf149
mehmetdelibas <m.metiin7@gmail.com>
2025-06-11 02:41:30 +0300

    regulerEx

---
+++ basicTerms/basicPhoneBook/regularEx.py
@@ -0,0 +1,89 @@
+import re
+
+# text = "Hello Python 3. Regular expressions in awsome!"
+
+# result = re.findall(r"\d",text)
+# if result:
+#     print("yes we have match")
+# else:
+#     print("no match")
+# print(result)
+
+# text = "Hello Python 3. Regular expressions in awsome!"
+
+# result = re.findall(r"P....n",text)
+# if result:
+#     print("yes we have match")
+# else:
+#     print("no match")
+# print(result)
+
+# text = "Hello Python 3. Regular expressions in awsome!"
+
+# result = re.findall(r"^H",text)
+# if result:
+#     print("yes we have match")
+# else:
+#     print("no match")
+# print(result)
+
+# text = "Hello Python 3. Regular expressions in awsome!"
+
+# result = re.findall(r"awsome!$",text)
+# if result:
+#     print("yes we have match")
+# else:
+#     print("no match")
+# print(result)
+
+# text = "Hello Python 3. Regular expressions in awsome!"
+
+# result = re.findall(r"He.*",text)
+# if result:
+#     print("yes we have match")
+# else:
+#     print("no match")
+# print(result)
+
+# text = "Hello Python 3. Regular expressions in awsome!"
+
+# result = re.findall(r"e.+t",text)
+# if result:
+#     print("yes we have match")
+# else:
+#     print("no match")
+# print(result)
+
+# while True:
+
+#     name = input("Enter a please name = ")
+
+#     if re.fullmatch(r"[a-zA-z]+",name):
+#         print("Thnks")
+#         break
+#     else:
+#         print("wrong")
+
+# while True:
+
+#     age = input("Enter a please age = ")
+
+#     if re.fullmatch(r"\d+",age):
+#         print("Thnks")
+#         break
+#     else:
+#         print("wrong")
+
+# def isValidEmail(email):
+#     pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+#     return re.fullmatch(pattern,email)
+
+# while True:
+
+#     email = input("Enter a please email = ")
+
+#     if isValidEmail(email):
+#         print("Thnks")
+#         break
+#     else:
+#         print("wrong")
