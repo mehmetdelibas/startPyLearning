63c74dac1b60011b2d65e44e024584194fb3a452
mehmetdelibas <m.metiin7@gmail.com>
2025-06-13 21:58:13 +0300

    finishPyLearn

---
+++ basicTerms/basicPhoneBook/numpyTest.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+sayilar = np.array([1,2,3])
+print(sayilar.dtype)
+
+print(np.zeros((2,3)))
+print(np.ones((3,3)))
+print(np.full((2,2),5))
+print(np.eye(3))
+print(np.arange(0,10,2))
